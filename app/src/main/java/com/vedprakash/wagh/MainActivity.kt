e6be36bdc186d234fb8ed1035a31fe816826021a
Vedprakash Wagh <tigerwed@gmail.com>
2022-07-22 15:43:52 +0530

    Third day

--- app/src/main/java/com/vedprakash/wagh/MainActivity.kt
+++ app/src/main/java/com/vedprakash/wagh/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.vedprakash.wagh
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.list_item_chat)
     }
 }
